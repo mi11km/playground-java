4e1c0d97523b229136cdedb856d12c28f0efc414
mi11km <mi11km.basket@gmail.com>
2022-05-11 15:20:35 +0900

    assignment2

--- app/src/main/java/playground/micsj3/Assignment2.java
+++ app/src/main/java/playground/micsj3/Assignment2.java
@@ -1,18 +1,45 @@
 package playground.micsj3;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-class NewFrame extends JFrame {
-    public NewFrame() {
-        this.setTitle("New Frame");
+class OmikujiFrame extends JFrame implements ActionListener {
+    private final JLabel label;
+
+    public OmikujiFrame() {
+        this.setTitle("Omikuji Frame");
         this.setSize(1200, 800);
-        this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton b = new JButton("おみくじを引く");
+        this.add(b, BorderLayout.SOUTH);
+        label = new JLabel("おみくじ", JLabel.CENTER);
+        this.add(label, BorderLayout.CENTER);
+        b.addActionListener(this);
+
+        this.setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        double r = Math.random();
+        if (r > 0.7) {
+            label.setText("Good!");
+        } else if (r > 0.2) {
+            label.setText("So so");
+        } else {
+            label.setText("Bad...");
+        }
+    }
+
+    public static void main(String[] args) {
+        new OmikujiFrame();
     }
 }
 
 public class Assignment2 {
     public void execute() {
-        new NewFrame();
+        new OmikujiFrame();
     }
 }
